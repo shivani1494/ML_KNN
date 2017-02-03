3aceedebbcb95691719ab8d0d31247d2f71561a5
shivani1494 <shivanimall1494@gmail.com>
2017-02-02 23:38:22 -0800

    projection_knn

--- knn_classifier.py
+++ knn_classifier.py
@@ -4,43 +4,117 @@ import math
 import sys
 import matplotlib.pyplot as plt
 import random
-
+import Queue as Q
 class KNN:
 
 	def __init__(sf):
 		file = open("train.txt")
-		train_data = file.readlines()
-		train_data = [x.strip() for x in train_data]
+		sf.train_data = file.readlines()
+		print "sf.train_data: ", sf.train_data		
+		for x in sf.train_data:
+			x.strip()
+			x = x.split()
+			x = np.array(x)
+			x.astype(int)
+			#print "x: ", x
 		
+		sf.train_data = [x.strip() for x in sf.train_data]
+		
+		for x in range(len(sf.train_data)):
+			
+
+		sf.train_data = np.array(sf.train_data)
+		
+		print sf.train_data
 		#print train_data
 	
 		file = open("test.txt")
-		test_data = file.readlines()
-		test_data = [x.strip() for x in test_data]
+		sf.test_data = file.readlines()
 
+		for x in sf.test_data:
+			x.strip()
+			x = x.split()
+			x = np.array(x)
+			x.astype(int)
+			print "x: ", x
+		
+		sf.test_data = [x for x in sf.test_data]
+		sf.test_data = np.array(sf.test_data)
+		
 	
 		#print test_data
 						
 		file = open("validate.txt")
-		validate_data = file.readlines()
-		validate_data = [x.strip() for x in validate_data]
-
+		sf.validate_data = file.readlines()
+		sf.validate_data = [x.strip() for x in sf.validate_data]
+		sf.validate_data = np.array(sf.validate_data)
+		sf.validate_data.astype(int)		
 
 		#print validate_data
 						
-
-	def one_knn(sf):
-
-	def five_knn(sf):
-
-	def nine_knn(sf):
-
-	def fifteen_knn(sf):
-
 	
+	def one_knn(sf):
+		label = sf.knn_computation(1)
+		print label
+	def five_knn(sf):
+		label = sf.knn_computation(5)
+	
+	def nine_knn(sf):
+		label = sf.knn_computation(9)
+	
+	def fifteen_knn(sf):
+		label = sf.knn_computation(15)
+	
+	def knn_computation(sf, k, data):		
 		
+		minDist = -1
+		label = -1
+		if(k == 0):
+			minDist = INT_MAX
+		else:
+			k_q = Q.PriorityQueue()
+			
+		
+		for td in range(len(data)):
+			for t in range(len(sf.train_data)):		
+				euclidean_dist = sf.eucledian_distance(sf.train_data[t], data[td])		
+				if( not k ):
+					if(minDist > euclidean_dist):
+						minDist = euclidean_dist
+						label = sf.train_data[t][784]
+				else:
+					if(k_q.size() == k):
+						dist_top = k_q.get()[0]
+						#make sure this logic is working well
+						if(dist_top < (-1*euclidean_dist)):
+							k_q.pop()
+					k_q.put((-1*euclidean_dist, sf.train_data[t][784]))
 
+					#do some computation here for k nearest neighbors
+
+		return label
+	
+	def eucledian_distance(sf, x1, x2):
+		print "x1", x1
+		print "x2", x2
+		return np.sqrt(sum((x1-x2)**2));	
+			
+	def run_knn(sf):
+		sf.knn_computation(1, sf.test_data)
+
+		#sf.knn_computation(5)
+
+		#sf.knn_computation(9)
+
+		#sf.knn_computation(15)
+	
+	#def calc_training_error(sf):
+
+	#def calc_testing_error(sf):
+	
+	#def calc_validation_error(sf):
 
 if __name__ == '__main__':
 	
 	knn = KNN()
+	knn.run_knn()
---
+++ projection_knn.py
@@ -0,0 +1,145 @@
+import numpy as np
+from random import choice
+
+def read_data(filename, projection = False):
+    try:
+        filehandle = open(filename,"r")
+        file = filehandle.read()
+        file = file.split()
+        data = []
+        if projection:
+            for row in range(len(file)//20):
+                data += [file[row*20:row*20+20]]
+            P = np.array(data)
+            return P.astype(float)
+        else:
+            for vector in range(len(file)//785):
+                data += [file[vector*785:vector*785+785]]
+
+            darray = np.array(data)
+            X = darray[:,0:784]
+            Y = darray[:,784]
+            return X.astype(int), Y.astype(int)
+        
+    except IOError:
+        print("IOError -- File does not exist")
+        return
+
+
+#X, Y = read_data("hw2train.txt")
+#XTest, YTest = read_data("hw2validate.txt")
+
+
+def knn(k, X, Y,XTest, num_classes):
+    predictions = []
+    for test_i in range(XTest.shape[0]):
+        closest_classes = [-1]*k
+        closest_dist = [-1]*k
+
+        for i in range(X.shape[0]):
+            dist = euclidean_dist(XTest[test_i],X[i])
+            replace_ind = None
+
+            if min(closest_dist) == -1:
+                replace_ind = closest_dist.index(min(closest_dist))
+            elif  max(closest_dist) > dist:
+                replace_ind = closest_dist.index(max(closest_dist))
+            if replace_ind != None:
+                closest_dist[replace_ind] = dist
+                closest_classes[replace_ind] = Y[i]
+        class_count = [0]*num_classes
+        for c in closest_classes:
+            class_count[c] += 1
+        # If tie, break randomly
+        if class_count.count(max(class_count)) > 1:
+            max_index = []
+            for i in range(len(class_count)):
+                if class_count[i] == max(class_count):
+                    max_index += [i]
+            c = choice(max_index)
+        else :
+            c = class_count.index(max(class_count))
+        predictions += [c]
+    return predictions
+                    
+        
+def euclidean_dist(v1,v2):
+    return np.sqrt(sum((v1-v2)**2))
+
+def test():
+    Y = np.array([1,1,1,1,1,2,2,2,2])
+    X = np.array([[1,2],[1,0],[1,1],[0,0],[0,-1],[-1,0],[-1,-1],[-1,-2],[-2,-2]])
+    XTest = np.array([[0,0],[1,1],[-1,-2]])
+    YTest = np.array([1,1,2])
+    results = (knn(3, X, Y, XTest, 10))
+    print(results, YTest)
+
+def t():
+    X,Y = read_data("hw2train.txt")
+    XTest, YTest = read_data("hw2validate.txt")
+    results = knn(3, X,Y, X, 10)
+    print(float((Y.shape[0] -( np.count_nonzero(np.array(results) ==  Y))))/Y.shape[0])
+
+    print(results, Y)
+
+#t()
+
+
+
+def projection():
+
+    	X,Y = read_data("hw2train.txt")
+    	XValid, YValid = read_data("hw2validate.txt")
+    	XTest, YTest = read_data("hw2test.txt")
+	P = read_data("projection.txt", True)
+
+	P_train = [np.dot(x, P) for x in X]
+	P_valid = [np.dot(x, P) for x in XValid]
+	P_test = [np.dot(x, P) for x in XTest]
+
+	P_train = np.array(P_train)
+	P_valid = np.array(P_valid)
+	P_test = np.array(P_test)
+
+	res3 = knn(3, P_train, Y, P_train, 10)
+    	err_train = float((Y.shape[0] -( np.count_nonzero(np.array(res3) ==  Y))))/Y.shape[0]
+	print err_train
+		
+	'''
+	
+	#validation
+	res1 = knn(1, P_train, Y, P_valid, 10)
+	res5 = knn(5, P_train, Y, P_valid, 10)
+	res3 = knn(3, P_train, Y, P_valid, 10)
+	res9 = knn(9, P_train, Y, P_valid, 10)
+	res15 = knn(15, P_train, Y, P_valid, 10)
+	
+	err = [0, 0, 0, 0]	
+    	err[0] = float((YValid.shape[0] -( np.count_nonzero(np.array(res1) ==  YValid))))/YValid.shape[0]
+    	err[1] = float((YValid.shape[0] -( np.count_nonzero(np.array(res5) ==  YValid))))/YValid.shape[0]
+    	err[2] = float((YValid.shape[0] -( np.count_nonzero(np.array(res9) ==  YValid))))/YValid.shape[0]
+    	err[3] = float((YValid.shape[0] -( np.count_nonzero(np.array(res15) ==  YValid))))/YValid.shape[0]
+
+	minIdx = err.index(min(err))
+	best_label = 0
+
+	if(minIdx == 0):
+		best_label = 1
+	elif(minIdx == 0):
+		best_label = 5
+	elif(minIdx == 0):
+		best_label = 9
+	else:
+		best_label = 12
+
+	print "validation error", err[minIdx]
+
+	res9 = knn(best_label, P_train, Y, P_test, 10)
+
+    	err_test = float((YTest.shape[0] -( np.count_nonzero(np.array(res9) ==  YTest))))/YTest.shape[0]
+
+	print "test error", err_test
+	
+	'''
+
+projection()
