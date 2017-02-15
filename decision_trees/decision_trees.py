fc561c1a20b22567c965e37c7f359e2661f83598
aitor <aitorolloqui@hotmail.com>
2017-02-15 07:27:18 +0000

    update

--- decision_trees/decision_trees.py
+++ decision_trees/decision_trees.py
@@ -29,22 +29,25 @@ class decision_tree:
 
         current_node = sf._rootnode
         while True:
-            if current_node.pure:
+            if not(current_node.pure):
                 feature, threshold = sf.maximize_IG(current_node.data)
                 split(current_node, feature, threshold)
 
-                if current_node.leftchild.pure:
-                    current_node = current_node.leftchild
-                elif current_node.rightchild.pure:
-                    current_node = current_node.rightchild
-                else:
-                    break
+                current_node = current_node.leftchild
 
+            elif current_node.parent != None:
+                if current_node.parent.rightchild != current_node:
+                    current_node = current_node.rightchild
+                elif current_node.parent.parent != None:
+                    pass
 
     def split(sf, node, feature, threshold):
         ind_greater, ind_smaller = sf.examples_threshold(node.data,feature,threshold)
-        node_smaller = Node(node, node.data[ind_smaller],"left")
-        node_greater = Node(node, node.data[ind_greater],"right")
+        if not(sf.feature_in_branch(node,feature)):
+            node_smaller = Node(node, node.data[ind_smaller], feature, threshold, "left")
+            node_greater = Node(node, node.data[ind_greater], feature, threshold, "right")
+        else :
+            pass # Choose label of the majority
 
 
     def maximize_IG(sf,X):
@@ -91,11 +94,24 @@ class decision_tree:
     def get_threshold(sf,X,feature):
         return np.mean(X[:,feature],axis=0)
 
+    def feature_in_branch(sf, node, feature):
+        # Checks if a feature is already being used in the branch
+        if (node.parent != None):
+            if node.parent.feature == feature:
+                return False
+            else :
+                return feature_in_branch(node.parent, feature)
+        else :
+            return True
+
     class Node:
-        def __init__(sf, parent, data, pos):
+        def __init__(sf, parent, data, feature, threshold, pos):
             sf.rightchild = None
             sf.leftchild = None
             sf.data = data
+            sf.label = None
+            sf.feature = feature
+            sf.threshold = threshold
             sf.pure = sf.check_purity()
             if parent:
                 sf.parent = parent
@@ -106,4 +122,10 @@ class decision_tree:
 
         def check_purity():
             same_label_count = np.count_nonzero(sf.data[:,sf._label_index])
-            return same_label_count == 0 or same_label_count == sf.data.shape[0]
+            if same_label_count == 0:
+                sf.label = 1
+            elif same_label_count == sf.data.shape[0]:
+                sf.label = 0
+            else :
+                return False
+            return True
