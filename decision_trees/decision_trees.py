59dcd7bed33c0ab42a2167c71c23fbf2c6081435
aitor <aitorolloqui@hotmail.com>
2017-02-16 05:38:22 +0000

    Some bugs fixed...

--- decision_trees/decision_trees.py
+++ decision_trees/decision_trees.py
@@ -1,14 +1,15 @@
 import numpy as np
 
 class DecisionTree:
-    def __init__(sf, filenames):
-        sf._label_index = 0
+    def __init__(sf, filenames, input_data=None):
         sf._num_features = 0
-        sf._input_data = load(filenames[0])
-
-        sf._feature_name = sf.feature_names("hw3features.txt")
+        if input_data != None:
+            sf._input_data = input_data
+            sf._num_features = input_data.shape[1]
+        else:
+            sf._input_data = sf.load(filenames[0])
+            sf._feature_names = sf.get_feature_names("hw3features.txt")
         sf._rootnode = None
-        sf.train()
 
     def load(sf,filename):
         print("Loading %s ..." %filename)
@@ -24,51 +25,79 @@ class DecisionTree:
         data = np.array(data)
         data = data.astype(float)
 
-        sf._label_index = data.shape[1]-1
-        sf._num_features = sf._label_index
-
         print(filename,"loaded : Dim",data.shape)
         return data
 
     def train(sf):
         print("Start training")
-        sf._rootnode = Node(None, sf._input_data, None)
+        sf._rootnode = Node(sf._input_data, None, None)
         current_node = sf._rootnode
         while True:
-            print(current_node)
             if not(current_node.pure):
-                feature, threshold = sf.maximize_IG(current_node.data)
-                split(current_node, feature, threshold)
+                feature, threshold = sf.maximize_IG(current_node)
+                if feature == None and threshold == None:
+                    # All the features used in that branch
+                    current_node.pure = True
 
-                current_node = current_node.leftchild
+                    # Actual label the all the data in current_node 
 
-            elif current_node.parent != None:
+                    label_index = current_node.data.shape[1]-1
+                    nonzero = np.count_nonzero(current_node.data[:,label_index])
+                    if (nonzero > current_node.data[:,label_index].shape[0]-nonzero):
+                        current_node.label_if_less = 1
+                    else:
+                        current_node.label_if_less = 0
+                    continue
+
+                current_node.threshold = threshold
+                current_node.feature = feature
+
+                sf.split(current_node, feature, threshold)
+                # Set current node to pure
+                # (we are not going to compute the tree for this node again)
+                current_node.pure = True
+
+                if current_node.leftchild:
+                    # Feature not already in branch, it has children
+                    # Build Left branch
+                    current_node = current_node.leftchild
+                print(current_node)
+                
+
+            elif current_node.parent:
                 if current_node.parent.rightchild != current_node:
+                    # Build sibling (rightchild) branch
                     current_node = current_node.rightchild
                 elif current_node.parent.parent != None:
-                    pass
+                    # Go to grandfather
+                    current_node = current_node.parent
+
+            else:
+                break
 
     def split(sf, node, feature, threshold):
-        # Creates two branches if the feature was not already used
+        # Creates two branches
+        label = node.data.shape[1]-1
         ind_greater, ind_smaller = sf.examples_threshold(node.data,feature,threshold)
-        if not(sf.feature_in_branch(node,feature)):
-            node_smaller = Node(node, node.data[ind_smaller], feature, threshold, "left")
-            node_greater = Node(node, node.data[ind_greater], feature, threshold, "right")
-        else :
-            pass # Choose label of the majority
+        
+        node_smaller = Node(node.data[ind_smaller,:], node, "left")
+        node_greater = Node(node.data[ind_greater,:], node, "right")
 
 
-    def maximize_IG(sf,X):
+    def maximize_IG(sf,node):
+        X = node.data
         max_ig = None
-        feature = 0
-        threshold = 0
+        feature = None
+        threshold = None
         for f in range(sf._num_features):
-            t = sf.get_threshold(X,f)
-            ig = sf.information_gain(X,f,t)
-            if max_ig == None or ig > best_ig:
-                best_ig = ig
-                feature = f
-                threshold = t
+            # Checks if the feature is already in use in the branch
+            if not(sf.feature_in_branch(node,f)):
+                t = sf.get_threshold(X,f)
+                ig = sf.information_gain(X,f,t)
+                if max_ig == None or ig > best_ig:
+                    best_ig = ig
+                    feature = f
+                    threshold = t
         return feature, threshold
 
     def information_gain(sf, X, feature, threshold):
@@ -77,27 +106,30 @@ class DecisionTree:
 
 
     def entropy(sf, X):
-        nonzero = np.count_nonzero(X[:,sf._label_index])
-        num_examples = data.shape[0]
+        # Log(0) = -Inf
+        #print(X)
+        label_index = X.shape[1]-1
+        nonzero = np.count_nonzero(X[:, label_index])
+        num_examples = X.shape[0]
 
-        return -nonzero/sf._num_examples*np.log(nonzero/sf._num_examples) -(sf._num_examples-nonzero)/sf._num_examples*np.log((sf._num_examples-nonzero)/sf._num_examples)
+        return -nonzero/num_examples*np.log(nonzero/num_examples) -(num_examples-nonzero)/num_examples*np.log((num_examples-nonzero)/num_examples)
 
     def conditional_entropy(sf, X, feature, threshold):
-        num_examples = data.shape[0]
-        ind_greater, ind_smaller = sf.examples_greater_than_threshold(X, feature, threshold)
+        num_examples = X.shape[0]
+        ind_greater, ind_smaller = sf.examples_threshold(X, feature, threshold)
 
-        greater = entropy(X[ind_greater,:]) # H(X,Z = 0)
-        smaller = entropy(X[ind_smaller,:]) # H(X,Z = 1)
+        greater_entropy = sf.entropy(X[ind_greater,:]) # H(X,Z = 0)
+        smaller_entropy = sf.entropy(X[ind_smaller,:]) # H(X,Z = 1)
 
-        prob_greater = len(greater)/sf._num_examples # P(Z=0)
-        prob_smaller = len(smaller)/sf._num_examples # P(Z=1)
+        prob_greater = X[ind_greater,:].shape[0]/num_examples # P(Z=0)
+        prob_smaller = X[ind_smaller,:].shape[0]/num_examples # P(Z=1)
 
-        return prob_greater * greater + prob_smaller * smaller
+        return prob_greater * greater_entropy + prob_smaller * smaller_entropy
 
     def examples_threshold(sf, X, feature, threshold):
         greater =  X[:,feature] > threshold # samples Z = 0
         smaller = X[:,feature] <= threshold # samples Z = 1
-        return np.where(greater), np.where(smaller)
+        return np.where(greater)[0], np.where(smaller)[0]
 
     def get_threshold(sf,X,feature):
         return np.mean(X[:,feature],axis=0)
@@ -106,24 +138,31 @@ class DecisionTree:
         # Checks if a feature is already being used in the branch
         if (node.parent != None):
             if node.parent.feature == feature:
-                return False
+                return True
             else :
-                return feature_in_branch(node.parent, feature)
+                return sf.feature_in_branch(node.parent, feature)
         else :
-            return True
+            return False
 
-    def feature_names(sf, filename):
-        filehandle = open(filenam, "r")
+    def get_feature_names(sf, filename):
+        filehandle = open(filename, "r")
+        feature_names = {}
+        index = 0
+        for line in filehandle:
+            feature_names[index] = line
+            index += 1
+        sf._num_features = index
 
-    class Node:
-        def __init__(sf, parent, data, feature, threshold, pos):
+class Node:
+        def __init__(sf, data, parent, pos):
             sf.rightchild = None
             sf.leftchild = None
             sf.data = data
-            sf.label = None
-            sf.feature = feature
-            sf.threshold = threshold
+            sf.label_if_less = None
+            sf.feature = None
+            sf.threshold = None
             sf.pure = sf.check_purity()
+            sf.parent = None
             if parent:
                 sf.parent = parent
                 if pos == "left":
@@ -132,18 +171,44 @@ class DecisionTree:
                     sf.parent.rightchild = sf
 
         def __str__(sf):
-            p = sf.parent.feature if p.parent else None
-            return "f<%i> p<%i>" %(f,p)
+            p = sf.parent.feature if sf.parent else None
+            return "f<%s> <= t<%s> : l<%s> || p<%s>" %(sf.feature, sf.threshold, sf.label_if_less, p)
 
-        def check_purity():
-            same_label_count = np.count_nonzero(sf.data[:,sf._label_index])
+        def check_purity(sf):
+            # All data has the same label, so the label is going to be the same as the data
+            same_label_count = np.count_nonzero(sf.data[:,sf.data.shape[1]-1])
             if same_label_count == 0:
-                sf.label = 1
+                sf.label_if_less = 1
             elif same_label_count == sf.data.shape[0]:
-                sf.label = 0
+                sf.label_if_less = 0
             else :
                 return False
             return True
 
 if __name__ == "__main__":
-    tree = DecisionTree()
+    # 2 x 6 matrix
+    # index 0 ==> feature
+    # index 1 ==> label
+    # threshold = 1
+    testX = np.array([[-1,1],[0,0],[2,1],[0,0],[3,1],[6,0]])
+
+    # 2 features
+    #testX = np.array([[1,1,1], [-1,-1,1], [-1,1,0], [1,-1,0]])
+    
+    tree = DecisionTree(None,testX)
+
+    # Check if feature_in_branch works
+    # Comment sf.pure in __init__ in Node
+    """n = Node([1], None, None)
+    n.feature = 5
+    n2 = Node([1],n, "left")
+    n3 = Node([1], n, "right")
+    n4 = Node([1], n2, "left")
+
+    print(tree.feature_in_branch(n4,4))"""
+
+    #tree = DecisionTree(["hw3train.txt"])
+    tree.train()
+
+    #print(tree.entropy(testX))
+    #print(tree.conditional_entropy(testX,0,1))
